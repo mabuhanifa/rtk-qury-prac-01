0f5d43d43831254b245fe051229f7efc99326933
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-31 01:17:24 +0600

    addEdit updated

--- src/pages/addEdit/AddEdit.js
+++ src/pages/addEdit/AddEdit.js
@@ -10,9 +10,49 @@ const AddEdit = () => {
   const [formValue, setFormValue] = useState(initialState);
   const { name, email, contact } = formValue;
   const navigate = useNavigate();
+  const handleInputChange = (e) => {};
+  const handleSubmit = (e) => {};
   return (
-    <div>
-      <h2>AddEdit</h2>
+    <div style={{ marginTop: "100px" }}>
+      <form
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="contact">Contact</label>
+        <input
+          type="number"
+          id="contact"
+          name="contact"
+          placeholder="Enter Contact Number"
+          value={contact}
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="Add" />
+      </form>
+      
     </div>
   );
 };
