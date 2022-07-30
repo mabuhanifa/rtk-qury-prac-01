9254baef0449bbebd2c97251a28f5e44aa828022
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-30 17:21:52 +0600

    todoQuery updated

---
+++ src/redux/rtk query/todoQuery.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const todoApi = createApi({
+  reducerPath: "todoApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getAllTodos: builder.query({
+      query: {
+        url: "todos",
+        method: "GET",
+      },
+    }),
+  }),
+});
+
+export const {useGetAllTodosQuery} = todoApi;
