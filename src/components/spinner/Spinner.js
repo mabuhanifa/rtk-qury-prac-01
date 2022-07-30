4add014278d40ba46cde21fe1da3ae302d7ec6b5
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-30 16:20:01 +0600

    spiner added

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="center">
+         <div class="ring"></div>
+         <span>loading...</span>
+      </div>
+    );
+};
+
+export default Spinner;
