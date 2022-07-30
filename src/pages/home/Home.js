462d45d0fee9a3597c63c14695a175d36210305d
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-31 00:24:11 +0600

    data fetched successfully

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,9 @@
+import { useContactsQuery } from '../../redux/rtk query/contactsApi';
 import './home.css';
 
 const Home = () => {
+    const contacts = useContactsQuery();
+    console.log(contacts);
     return (
         <div>
             home
